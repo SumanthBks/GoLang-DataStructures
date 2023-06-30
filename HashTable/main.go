4679fae5404dba1d587d453ad622b07e65a3c2eb
SumanthBks <bkssumanth@gmail.com>
2023-06-30 19:35:30 +0530

    Hashtable with insert, delete, search

--- HashTable/main.go
+++ HashTable/main.go
@@ -12,57 +12,121 @@ import "fmt"
 // and the data is pilled up in the linked list under the bucket
 // when the last element in the linked list is searched then performance hit happens
 
-// Arraysize
-const Arraysize = 10
+const arraySize = 10
 
-// hashtable
 type HashTable struct {
-	array [Arraysize]*buckets
+	array [arraySize]*Buckets
 }
 
-// Buckets
-type buckets struct {
-	head *linkedlist
+type Buckets struct {
+	head *LinkedList
 }
 
-// linkedlist
-type linkedlist struct {
+type LinkedList struct {
 	key  string
-	next *linkedlist
+	next *LinkedList
 }
 
-// insert into array and bucket
 func (h *HashTable) insert(key string) {
 	index := hash(key)
-	h.array[index].bucketInsert(key)
+	h.array[index].insertBucket(key)
+	fmt.Println(key)
 }
 
-// insert into the bucket and into the linked list
-func (b *buckets) bucketInsert(k string) {
-	// TODO: check the below linked list structure and understand
-	newNode := &linkedlist{key: k}
-	newNode.next = b.head
-	b.head = newNode
+func (b *Buckets) insertBucket(key string) {
+	if !b.searchBucket(key) {
+		newNode := &LinkedList{key: key} // initilize the linkedlist
+		newNode.next = b.head            // add the head of the linkedlist to the next of the newNode
+		b.head = newNode                 // make the newNode as the head of the linkedlist
+	} else {
+		fmt.Println("Key exists: ", key)
+	}
+}
+
+func returnIndex(key string) int {
+	return hash(key)
+}
+
+func (h *HashTable) delete(key string) {
+	index := returnIndex(key)
+	h.array[index].deleteKey(key)
+}
+
+func (b *Buckets) deleteKey(key string) {
+	if b.searchBucket(key) {
+		if b.head.key == key {
+			b.head = b.head.next // if the head is the target deletion then make head.next as the head
+			return
+		}
+
+		currentNode := b.head
+		for currentNode.next != nil {
+			if currentNode.next.key == key {
+				currentNode.next = currentNode.next.next // if the nest of currenNode is the target deletion then make the next of the next of the list the current.next
+			}
+			currentNode = currentNode.next
+		}
+
+	} else {
+		fmt.Println("Key Doesn't exists: ", key)
+	}
+}
+
+func (h *HashTable) search(key string) bool {
+	return h.array[returnIndex(key)].searchBucket(key)
+}
+
+func (b *Buckets) searchBucket(key string) bool {
+	currentNode := b.head
+	for currentNode != nil {
+		if currentNode.key == key {
+			return true
+		}
+		currentNode = currentNode.next
+	}
+	return false
 }
 
 func hash(key string) int {
-	hashSum := 0
+	sum := 0
 	for _, k := range key {
-		hashSum += int(k)
+		sum += int(k)
 	}
-	return hashSum % Arraysize
+	return sum % arraySize
 }
 
-func initialiseBucket() *HashTable {
+func initialize() *HashTable {
 	ht := &HashTable{}
-	for i := range ht.array {
-		ht.array[i] = &buckets{}
+	for arr := range ht.array {
+		ht.array[arr] = &Buckets{}
 	}
 	return ht
 }
 
 func main() {
-	ht := initialiseBucket()
-	fmt.Println(ht)
-	ht.insert("SUMANTH")
+	ht := initialize()
+	listOfKeys := []string{
+		"Sumanth",
+		"Yuvan",
+		"Krishna",
+		"Basetty",
+		"Sai",
+		"Swathi",
+		"Pratyusha",
+		"Koushika",
+		"Srawan",
+		"Anurag",
+		"Hari",
+		"Mahesh",
+		"Swathi",
+		"Sumalatha",
+		"Sudhakar",
+	}
+
+	for _, key := range listOfKeys {
+		ht.insert(key)
+	}
+	fmt.Println(ht.search("Koushika"))
+	ht.delete("Koushika")
+	fmt.Println(ht.search("Koushika"))
 }
